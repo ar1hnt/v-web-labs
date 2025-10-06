862577cbff28c8ea50aee981f79083cfd4f6cd9a
Danil Aganin <161352827+ar1hnt@users.noreply.github.com>
2025-10-06 19:52:38 +0300

    Correct ESLint configuration syntax
    Fix syntax error in ESLint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+   Error: Cannot find module '/ browser: true,
+    es2021: true,
+ home/runner/work/v-web-labs/v-web-labs/.eslintrc.js'
+
+To fix this, add a },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions .eslintrc.js file to the root of your repository.: {
+    ecmaVersion: 12,
+    source Here is a minimal example:
+
+```js
+module.exports = {
+Type: 'module',
+  },
+  rules: {},
+};
+env: {
+  browser: true,
+  es2021: true,
+},
+extends:  ```
+- Commit this file to your repository root and re ['eslint:recommended'],
+parserOptions: {
+  ecmaVersion:-run the workflow.
+
+Once the ESLint config is present 12,
+  sourceType: 'module',
+},
+rules: {},
+};
